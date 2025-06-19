7ada116be66944e4c03bdb9107b554be447503e7
htikelinn <htikezawlinn@gmail.com>
2025-06-19 11:02:27 +0630

    add Color and String Converter File

---
+++ 05-inherient.mappings/src/main/java/com/dos/hzl/converter/StringConverter.java
@@ -0,0 +1,19 @@
+package com.dos.hzl.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class StringConverter implements AttributeConverter<String, Integer>{
+
+	@Override
+	public Integer convertToDatabaseColumn(String attribute) {
+		return Integer.parseInt(attribute);
+	}
+
+	@Override
+	public String convertToEntityAttribute(Integer dbData) {
+		return dbData.toString();
+	}
+
+}
